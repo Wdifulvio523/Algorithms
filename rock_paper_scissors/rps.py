0fd72345061eb02d4db579848ffef592b97b1976
William DiFulvio <w.difulvio523@gmail.com>
2019-02-21 16:37:32 -0500

    completed rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,27 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
 
+  # define possible plays, and make list of  possibleoutcomes 
+  outcomes = [] 
+  plays = ['rock', 'paper', 'scissors']
+
+  # make helper function that can take the rounds left (n), and the result(empty [])
+  def helper(roundsLeft, result):
+
+    # if there are no rounds left, it will take all the results 
+    # and put it in the outcomes array
+    if roundsLeft == 0:
+      outcomes.append(result)
+      return
+    # as long as there are plays left, for every item in the plays array, 
+    # it will add it to the results array  
+    for play in plays:
+      helper(roundsLeft - 1, result + [play])
+
+  #  call the helper function, and return the outcomes array to get your answer
+  helper(n,[])
+  return outcomes
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
